4dfe59d4b30065c2d62ff42cf4a8fe71382f3f13
Xinyu Yang <x.yang.2@research.gla.ac.uk>
2021-07-20 10:28:06 +0800

    +Co_Occurece_Network_Analysis.Rmd
    Update

---
+++ extractLegend.R
@@ -0,0 +1,10 @@
+extractLegend <- function(gg) {
+  grobs <- ggplot_gtable(ggplot_build(gg))
+  foo <- which(sapply(grobs$grobs, function(x) x$name) == "guide-box")
+  grobs$grobs[[foo]]
+}
+
+# Extract wanted legend
+wantedLegend <- extractLegend(template)
+# ####### Conver igraph to ggraph
+
